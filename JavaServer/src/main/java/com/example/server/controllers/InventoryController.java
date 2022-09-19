e4177c3acccb7baf98726d02a14313cc1cf96096
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-19 12:38:51 -0700

    finished inventory controllers

--- JavaServer/src/main/java/com/example/server/controllers/InventoryController.java
+++ JavaServer/src/main/java/com/example/server/controllers/InventoryController.java
@@ -1,11 +1,44 @@
 package com.example.server.controllers;
 
+import com.example.server.models.Inventory;
+import com.example.server.servicies.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/inventory")
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class InventoryController {
+
+    @Autowired
+    private InventoryService inventoryService;
+
+    @GetMapping("/getItems")
+    @ResponseBody
+    public List<Inventory> getAllInventory() {
+        return inventoryService.getAllInventory();
+    }
+
+    @GetMapping("/item/{id}")
+    @ResponseBody
+    public Optional<Inventory> getOneInventoryItem(@PathVariable long id) {
+        return inventoryService.getOneInventoryItem(id);
+    }
+
+    @PostMapping("/saveItem")
+    @ResponseBody
+    public String addItem(@RequestBody Inventory inventory) {
+        inventoryService.saveInventory(inventory);
+        return "New Item added!";
+    }
+
+    @DeleteMapping("deleteItem/{id}")
+    @ResponseBody
+    public String deleteItem(@PathVariable Inventory id) {
+        inventoryService.deleteInventory(id);
+        return "Item deleted!";
+    }
 
 }
