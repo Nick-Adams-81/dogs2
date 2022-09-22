63425263be0e96de045460172e614b6026681ae7
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-22 16:12:47 -0700

    added sql file and migration script in example.properties

---
+++ JavaServer/src/main/resources/data.sql
@@ -0,0 +1,53 @@
+
+
+INSERT INTO candy_corp (item_name, cost)
+VALUES('Licorice', 0.81),
+      ('Good & Plenty', 0.46),
+      ('Smarties', 0.89),
+      ('Tootsie Rolls', 0.45);
+
+INSERT INTO dentisis_hate_us (item_name, cost)
+VALUES('Snickers', 0.47),
+      ('M & Ms', 0.84),
+      ('Skittles', 0.15),
+      ('Starburst', 0.07),
+      ('Butterfinger', 0.97),
+      ('Peach Rings', 0.39),
+      ('Gummy Bears', 0.91),
+      ('Sour Patch Kids', 0.85);
+
+INSERT INTO inventory (product_name, stock, capacity)
+VALUES('Licorice', 22, 25),
+      ('Good & Plenty', 4, 20),
+      ('Smarties', 15, 25),
+      ('Tootsie Rolls', 30, 50),
+      ('Necco Wafers', 14, 15),
+      ('Wax Cola Bottles', 8, 10),
+      ('Circus Peanuts', 10, 10),
+      ('Candy Corn', 30, 40),
+      ('Twix', 17, 70),
+      ('Snickers', 43, 65),
+      ('M & Ms', 32, 55),
+      ('Skittles', 25, 45),
+      ('Starburst', 8, 45),
+      ('Butterfinger', 10, 60),
+      ('Peach Rings', 20, 30),
+      ('Gummy Bears', 15, 35),
+      ('Sour Patch Kids', 14, 60);
+
+INSERT INTO the_sweet_suite (item_name, cost)
+VALUES('Good & Plenty', 0.18),
+      ('Smarties', 0.54),
+      ('Tootsie Rolls', 0.67),
+      ('Necco Wafers', 0.25),
+      ('Wax Cola Bottles', 0.35),
+      ('Circus Peanuts', 0.23),
+      ('Candy Corn', 0.41),
+      ('Twix', 0.54),
+      ('Snickers', 0.25),
+      ('M & Ms', 0.52),
+      ('Skittles', 0.07),
+      ('Starburst', 0.77),
+      ('Butterfinger', 0.93),
+      ('Peach Rings', 0.11),
+      ('Gummy Bears', 0.42);
